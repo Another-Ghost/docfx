1c0e4e4fa37c2703ec1ba87cb1b21b3a4fcc2d22
Bo Li <55052775+Another-Ghost@users.noreply.github.com>
2024-12-05 21:27:57 +0800

    MOD custom build step

---
+++ src/Docfx.Build.ManagedReference/BuildManagedReferenceDocumentCustom.cs
@@ -0,0 +1,54 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Immutable;
+using System.Composition;
+
+using Docfx.Build.Common;
+using Docfx.DataContracts.Common;
+using Docfx.DataContracts.ManagedReference;
+using Docfx.Plugins;
+
+namespace Docfx.Build.ManagedReference;
+
+[Export(nameof(ManagedReferenceDocumentProcessor), typeof(IDocumentBuildStep))]
+public class BuildManagedReferenceDocumentCustom : BaseDocumentBuildStep
+{
+    public override string Name => nameof(BuildManagedReferenceDocumentCustom);
+
+    public override int BuildOrder => -1;
+
+    public override void Build(FileModel model, IHostService host)
+    {
+        HandleXMLTag(model, host);
+    }
+
+    public override void Postbuild(ImmutableList<FileModel> models, IHostService host)
+    {
+
+    }
+
+    private void HandleXMLTag(FileModel model, IHostService host)
+    {
+        var pageViewModel = (PageViewModel)model.Content;
+        if(pageViewModel != null)
+        {
+            foreach(var item in pageViewModel.Items)
+            {
+                if (pageViewModel.Metadata.TryGetValue("name", out var name))
+                {
+                    pageViewModel.Metadata.Add("name", name);
+                }
+                // if(item.Summary != null)
+                // {
+                    item.Name += "CUSTOM";
+                    item.NameWithType += "CUSTOM";
+                    //item.Examples.Add("CUSTOM");
+                    //item.Names;
+                    // }
+            }
+        }
+
+
+    }
+}
